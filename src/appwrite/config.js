a940d2aa8b8079010ae6980aa63c7821a3fdc6b7
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2024-08-25 22:29:27 +0530

    login signup post update and delete

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-useless-catch */
-import conf from '../conf.js'
+import conf from '../conf/conf'
 import {Client, ID, Databases, Storage, Query} from 'appwrite'
 
 export class Service{
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,21 @@ import Footer from './Footer/Footer';
 import Container from './container/Container'
 import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn'
+import Input from './Input'
+import Select from './Select'
+import Buttom from './Button'
+import PostCard from './PostCard'
+import RTE from './RTE'
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Select,
+    Buttom,
+    PostCard,
+    RTE,
 }
