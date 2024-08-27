95e3090eef1d1fabf9d14378100ec1e5e27ae5f7
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2024-08-27 22:34:02 +0530

    created pages and routes

--- src/components/index.js
+++ src/components/index.js
@@ -5,9 +5,13 @@ import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn'
 import Input from './Input'
 import Select from './Select'
-import Buttom from './Button'
+import Button from './Button'
 import PostCard from './PostCard'
 import RTE from './RTE'
+import AuthLayout from './AuthLayout'
+import PostForm from './post-form/PostForm'
+import Login from './Login'
+import SignUp from './Signup'
 
 export {
     Header,
@@ -17,7 +21,11 @@ export {
     LogoutBtn,
     Input,
     Select,
-    Buttom,
+    Button,
     PostCard,
     RTE,
+    AuthLayout,
+    PostForm,
+    Login,
+    SignUp
 }
