fbc000433f25b11e325d96a08a893c0bcc591c62
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2024-12-24 10:03:21 +0530

    add loading spinner to Home component and refactor post fetching logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,73 @@
 import React, { useEffect, useState } from 'react'
 import appwriteService from '../appwrite/config'
-import {Container, PostCard} from '../components'
+import { Container, PostCard } from '../components'
+import { Grid } from 'react-loader-spinner'
 
 function Home() {
     const [posts, setPosts] = useState([])
+    const [isLoading, setLoading] = useState(true)
 
     useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if(posts){
-                setPosts(posts.documents)
+        const fetchPosts = async () => {
+           try {
+            const result = await appwriteService.getPosts();
+            if (result) {
+                setPosts(result.documents);
             }
-        })
+           } catch (error) {
+            console.log(error.message)
+           } finally{
+             setLoading(false);
+           }
+        }
+        fetchPosts();
     }, [])
 
-  if(posts.length === 0){
-    return (
-        <div className='w-full py-8 mt-4 text-center'>
-            <Container>
-                <div className='flex flex-wrap'>
-                    <div className='p-2 w-full'>
-                        <h1 className='text-2xl font-bold hover:text-gray-500'>Login to read posts content</h1>
+
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center h-96" aria-live="polite">
+                <Grid
+                    visible={true}
+                    height="100"
+                    width="100"
+                    color="#50727B"
+                    ariaLabel="grid-loading"
+                    radius="12.5"
+                    wrapperStyle={{}}
+                    wrapperClass="grid-wrapper"
+                />
+            </div>
+        );
+    }
+
+    if (posts.length === 0) {
+        return (
+            <div className='w-full py-8 mt-4 text-center'>
+                <Container>
+                    <div className='flex flex-wrap'>
+                        <div className='p-2 w-full'>
+                            <h1 className='text-2xl font-bold hover:text-gray-500'>Login to read posts content</h1>
+                        </div>
                     </div>
-                </div>
-            </Container>
-        </div>
-    )
-  }
-  return (
-    <div className='w-full py-8'>
+                </Container>
+            </div>
+        )
+    }
+    return (
+        <div className='w-full py-8'>
             <Container>
                 <div className='flex flex-wrap'>
                     {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
+                        <div key={post.$id} className='p-2 w-1/2 md:w-1/3 lg:w-1/4'>
                             <PostCard {...post} />
                         </div>
                     ))}
                 </div>
-                
+
             </Container>
         </div>
-  )
+    )
 }
 
 export default Home
