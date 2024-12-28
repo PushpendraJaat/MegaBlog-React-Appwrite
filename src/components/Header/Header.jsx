3cef3250d94c07a2c87dd8a14cd64e9fd1d9fcf1
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2024-12-28 21:01:17 +0530

    refactor Header and Post components; increase z-index for better layering

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
   ];
 
   return (
-    <header className='py-3 shadow bg-customdarkblue z-10'>
+    <header className='py-3 shadow bg-customdarkblue z-50 w-full sticky top-0'>
       <Container>
         <nav className='flex items-center justify-between'>
           {/* Logo */}
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -36,11 +36,11 @@ export default function Post() {
     return post ? (
         <div className="py-8">
             <Container>
-                <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+                <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2 z-1 ">
                     <img
                         src={appwriteService.getFilePreview(post.featuredImage)}
                         alt={post.title}
-                        className="rounded-xl"
+                        className="rounded-xl z-0"
                     />
              
                     {isAuthor && (
@@ -56,6 +56,7 @@ export default function Post() {
                         </div>
                     )}
                 </div>
+              
                 <div className="w-full mb-6">
                     <h1 className="text-2xl font-bold">{post.title}</h1>
                 </div>
